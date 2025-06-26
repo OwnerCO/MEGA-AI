41069f201aed74f0503d4464076743033ffd8e2b
OwnerCO <jazminqntro96@gmail.com>
2025-06-26 17:56:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '51941847465';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '51941847465' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,9 +20,9 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.mods = []
-global.prems = []
-global.allowed = ['923444844060', '923051391007']
+global.mods = ['51941847465']
+global.prems = ['51941847465']
+global.allowed = ['51941847465']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
